2009087809c8c2369096c552b47fc3f6a1bfb891
Patrick McKinney <pmacMaps@users.noreply.github.com>
2023-07-24 16:28:11 -0400

    Update constants.js

--- Code/Lessons/Lesson 13/js/constants.js
+++ Code/Lessons/Lesson 13/js/constants.js
@@ -2,5 +2,3 @@
 export const initCoords = [40.25, -76.88];
 // initial zoom
 export const initScale = 12;
-// esri api key
-export const esriKey = 'AAPK56c13fae1a6d4407a5d392981b9b50d2kwxUZMYHbCl1MyifP-JufC7lrC9K2TvvL93oR25h20j0cjvoAro30x-p0O27jPwS';
